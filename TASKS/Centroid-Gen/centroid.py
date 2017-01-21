8c83eca921e79da9d67590fb4863790b4bbd7d01
Soumya Vadlamannati <vadlamannati1997@gmail.com>
2017-01-21 11:50:32 +0530

    Updating centroid.py
    Adding input file option

--- TASKS/Centroid-Gen/centroid.py
+++ TASKS/Centroid-Gen/centroid.py
@@ -1,62 +1,71 @@
 from osgeo import ogr
 import os
+import sys
 
-# Get the input Layer
-inShapefile = "1roomr.shp"
-inDriver = ogr.GetDriverByName("ESRI Shapefile")
-inDataSource = inDriver.Open(inShapefile, 0)
-inLayer = inDataSource.GetLayer()
+def main():
+    argument = sys.argv
+        if not (len(argument)==2) :
+            print "Usage: python ExtractLine.py Filename"
+            inShapefile = "1roomr.shp"
+            # file to use if user doesnot provide input file
+        FileName = argument[1]
+    # Get the input Layer
 
-# Create the output Layer
-outShapefile = "centroids.shp"
-outDriver = ogr.GetDriverByName("ESRI Shapefile")
+    inDriver = ogr.GetDriverByName("ESRI Shapefile")
+    inDataSource = inDriver.Open(inShapefile, 0)
+    inLayer = inDataSource.GetLayer()
 
-# Remove output shapefile if it already exists
-if os.path.exists(outShapefile):
-    outDriver.DeleteDataSource(outShapefile)
+    # Create the output Layer
+    outShapefile = "centroids.shp"
+    outDriver = ogr.GetDriverByName("ESRI Shapefile")
 
-# Create the output shapefile
-outDataSource = outDriver.CreateDataSource(outShapefile)
-outLayer = outDataSource.CreateLayer("centroid", geom_type=ogr.wkbPoint)
+    # Remove output shapefile if it already exists
+    if os.path.exists(outShapefile):
+        outDriver.DeleteDataSource(outShapefile)
+
+    # Create the output shapefile
+    outDataSource = outDriver.CreateDataSource(outShapefile)
+    outLayer = outDataSource.CreateLayer("centroid", geom_type=ogr.wkbPoint)
 
 
-# Add input Layer Fields to the output Layer
-inLayerDefn = inLayer.GetLayerDefn()
-for i in range(0, inLayerDefn.GetFieldCount()):
-    fieldDefn = inLayerDefn.GetFieldDefn(i)
-    outLayer.CreateField(fieldDefn)
-idField = ogr.FieldDefn("Type", ogr.OFTString)
-outLayer.CreateField(idField)
-idField = ogr.FieldDefn("Geometry", ogr.OFTString)
-outLayer.CreateField(idField)
-idField = ogr.FieldDefn("Capacity", ogr.OFTInteger)
-outLayer.CreateField(idField)
-idField = ogr.FieldDefn("Impedence", ogr.OFTReal)
-outLayer.CreateField(idField)
-idField = ogr.FieldDefn("Distances", ogr.OFTInteger)
-outLayer.CreateField(idField)
-# Get the output Layer's Feature Definition
-outLayerDefn = outLayer.GetLayerDefn()
+    # Add input Layer Fields to the output Layer
+    inLayerDefn = inLayer.GetLayerDefn()
+    for i in range(0, inLayerDefn.GetFieldCount()):
+        fieldDefn = inLayerDefn.GetFieldDefn(i)
+        outLayer.CreateField(fieldDefn)
+    idField = ogr.FieldDefn("Type", ogr.OFTString)
+    outLayer.CreateField(idField)
+    idField = ogr.FieldDefn("Geometry", ogr.OFTString)
+    outLayer.CreateField(idField)
+    idField = ogr.FieldDefn("Capacity", ogr.OFTInteger)
+    outLayer.CreateField(idField)
+    idField = ogr.FieldDefn("Impedence", ogr.OFTReal)
+    outLayer.CreateField(idField)
+    idField = ogr.FieldDefn("Distances", ogr.OFTInteger)
+    outLayer.CreateField(idField)
+    # Get the output Layer's Feature Definition
+    outLayerDefn = outLayer.GetLayerDefn()
 
-# Add features to the ouput Layer
-for i in range(0, inLayer.GetFeatureCount()):
-    # Get the input Feature
-    inFeature = inLayer.GetFeature(i)
-    # Create output Feature
-    outFeature = ogr.Feature(outLayerDefn)
-    # Set geometry as centroid
-    geom = inFeature.GetGeometryRef()
-    centroid = geom.Centroid()
+    # Add features to the ouput Layer
+    for i in range(0, inLayer.GetFeatureCount()):
+        # Get the input Feature
+        inFeature = inLayer.GetFeature(i)
+        # Create output Feature
+        outFeature = ogr.Feature(outLayerDefn)
+        # Set geometry as centroid
+        geom = inFeature.GetGeometryRef()
+        centroid = geom.Centroid()
 
-    outFeature.SetGeometry(centroid)
-    outFeature.SetField("Type", "room-centroid")
-    outFeature.SetField("Geometry", "Polygon")
-    outFeature.SetField("Capacity", 10)
-    outFeature.SetField("Impedence", 0.34)
-    outFeature.SetField("Distances", 3)
-    # Add new feature to output Layer
-    outLayer.CreateFeature(outFeature)
+        outFeature.SetGeometry(centroid)
+        outFeature.SetField("Type", "room-centroid")
+        outFeature.SetField("Geometry", "Polygon")
+        outFeature.SetField("Capacity", 10)
+        outFeature.SetField("Impedence", 0.34)
+        outFeature.SetField("Distances", 3)
+        # Add new feature to output Layer
+        outLayer.CreateFeature(outFeature)
 
-# Close DataSources
-inDataSource.Destroy()
-outDataSource.Destroy()
+    # Close DataSources
+    inDataSource.Destroy()
+    outDataSource.Destroy()
+main()
