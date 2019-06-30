75c5f1936bbb181725c9d86cf90f6f42d6bbb710
adityacheruvu <adityacheruvucs@gmail.com>
2019-07-01 00:55:12 +0530

    Found staircases completely as a set of lists of midlanding lines and lists of steps

--- Code/Mycode/stairway.py
+++ Code/Mycode/stairway.py
@@ -7,6 +7,7 @@ from GenCenterLine import *
 from GenOutput import *
 from itertools import compress
 import pprint
+import math
 
 #-----------------------------------------
 #Parameters
@@ -101,8 +102,8 @@ class setOFSteps():
         
 
 class midLanding():
-    def __init__(self):
-        pass
+    def __init__(self, listOfLines):
+        self.listOfLines = listOfLines
 #-----------------------------------------
 
 #Utility Functions
@@ -196,6 +197,18 @@ def areLineSetsParallel(angle1, angle2):
         return True
     return False
 
+def areLineSetsPerpendicular(angle1, angle2):
+    """
+    Function to determine if 2 line sets can be considered perpendicular.
+    angle1, angle2: degree of rotation in radians of 2 line sets.
+    """
+    diff = abs(angle1 - angle2)
+    if abs(diff - math.pi/2) < RadianEPS :
+        return True
+    if abs(abs(diff - math.pi)-math.pi/2) < RadianEPS :
+        return True
+    return False
+
 def findGroupsOfAngles(allAnglesList, allAnglesCount):
     """
     groups different angles of lines found in the drawing, if they can be considered parallel.
@@ -236,8 +249,49 @@ def findConnectionBetweenParallelFlightsOfStairs(flight1, flight2):
         line1 = Segment(flight2.bounding_rect[0], Segment(flight1.bounding_rect[2], flight1.bounding_rect[3]).projection(flight2.bounding_rect[0]))
         line2 = Segment(flight2.bounding_rect[1], Segment(flight1.bounding_rect[2], flight1.bounding_rect[3]).projection(flight2.bounding_rect[1]))
     
-    return (line1, line2)
+    boundingLine1 = Segment(flight1.bounding_rect[2], flight1.bounding_rect[3])
+    boundingLine2 = Segment(flight2.bounding_rect[0], flight2.bounding_rect[1])
+    return (line1, line2, boundingLine1, boundingLine2)
 
+def searchLinesInSearchSpace(linesToSearch, searchSpace):
+    """
+    linesToSearch: list of lines assumed virtually, to be searched in the searchSpace
+    searchSpace: list of lines extracted from drawing to search in
+
+    Returns a list of lines with all lines found in the search, and those which are not found in the search.
+    """
+    listToReturn = []
+    for L in searchSpace:
+        i=0
+        while(i<len(linesToSearch)):
+            if(linesToSearch[i].a == L.a and linesToSearch[i].b == L.b):
+                listToReturn.append(linesToSearch[i])
+                linesToSearch.pop(i)
+            i+=1
+    listToReturn+=linesToSearch
+    return listToReturn
+
+def areaOfTriangle(a, b, c):
+    return (abs(a.x*(b.y-c.y) + b.x*(c.y-a.y) + c.x*(a.y-b.y)))/2
+
+def closeMidlanding(referenceLine, referencePoint, d):
+    perpEqs = getPerpendicularLineEqs(referenceLine.slope, referenceLine.a, referenceLine.b)
+    referenceLineEq = (referenceLine.slope, referenceLine.a.y - referenceLine.slope*referenceLine.a.x)
+    substitutedReferenceValue = referencePoint.y - referenceLineEq[0]*referencePoint.x - referenceLineEq[1]
+
+    a, b = referenceLine.a, referenceLine.b
+    theta = referenceLine.angle
+    perpendicularPointPair1 = (Point(a.x + d*sin(theta), a.y - d*cos(theta)), Point(b.x + d*sin(theta), b.y - d*cos(theta)))
+    perpendicularPointPair2 = (Point(a.x - d*sin(theta), a.y + d*cos(theta)), Point(b.x - d*sin(theta), b.y + d*cos(theta)))
+    substitutedValue1 = perpendicularPointPair1[0].y - referenceLineEq[0]*perpendicularPointPair1[0].x - referenceLineEq[1]
+    substitutedValue2 = perpendicularPointPair2[0].y - referenceLineEq[0]*perpendicularPointPair2[0].x - referenceLineEq[1]
+    if(substitutedReferenceValue*substitutedValue1 < 0):
+        pair = perpendicularPointPair1
+    else:
+        pair = perpendicularPointPair2
+    
+    perpendicularSegments = [Segment(pair[0], referenceLine.a), Segment(pair[1], referenceLine.b), Segment(pair[0], pair[1])]
+    return perpendicularSegments
 
 #-----------------------------------------
 
@@ -252,6 +306,8 @@ if __name__ == "__main__":
     dwg = ezdxf.readfile(inputFile)
     modelspace = dwg.modelspace()
     LineSegmentsFromDwg = [Segment(Point(*line.dxf.start[:-1]), Point(*line.dxf.end[:-1])) for line in modelspace.query('LINE')]
+    print("List of original lines has size, ", len(LineSegmentsFromDwg))
+
     """
     i=0
     while(i<len(LineSegmentsFromDwg)):
@@ -401,12 +457,19 @@ if __name__ == "__main__":
             parallelLineSets[k] = list(set(parallelLineSets[k])-set(listOfStaircases[i]))
             parallelLineSets[k].sort(key=keyToSortLines)
     print(parallelLineSets.keys())
+
+    LineSegmentsFromDwg = set(LineSegmentsFromDwg)
+    for i in listOfStaircases:
+        LineSegmentsFromDwg = LineSegmentsFromDwg - set(i)
+    RemainingSearchSpace = list(LineSegmentsFromDwg)
+
+    print("List of lines has size: ", len(LineSegmentsFromDwg))
+    print("List of Remaining search space has size: ", len(RemainingSearchSpace))
+
     for i in range(len(listOfStaircases)):
         listOfStaircases[i] = setOFSteps(listOfStaircases[i])
-        
-        
-        #MakeShapeFile(listOfStaircases[i], "stairsNo"+str(i)+".shp")
     
+
     #Club all the continuois sets of stairs
     i=0
     j=1
@@ -425,20 +488,167 @@ if __name__ == "__main__":
     print
     print(listOfStaircases[1].listOfLines, len(listOfStaircases[1].listOfLines))"""
 
+    pairs = []
+    midLandings = []
     for i in range(len(listOfStaircases)):
         for j in range(i+1, len(listOfStaircases)):
+            midLand = []
             if(areLineSetsParallel(listOfStaircases[i].avgAngleOfRotation , listOfStaircases[j].avgAngleOfRotation)):
                 boundingBox1 = listOfStaircases[i].bounding_rect
                 boundingBox2 = listOfStaircases[j].bounding_rect
                 line1 = Segment(boundingBox1[0], boundingBox1[1])
                 line2 = Segment(boundingBox2[0], boundingBox2[1])
+                midLandingWidth = min(listOfStaircases[i].stepLength, listOfStaircases[j].stepLength)
+                maxMidLandingLength = min(listOfStaircases[i].runningLength, listOfStaircases[j].runningLength)
                 if(abs(min(line1.length, line2.length) - line1.projection(line2).length) <= EPS):
-                    ConnectingLine1, ConnectingLine2 = findConnectionBetweenParallelFlightsOfStairs(listOfStaircases[i], listOfStaircases[j])
+                    ConnectingLine1, ConnectingLine2, boundingLine1, boundingLine2 = findConnectionBetweenParallelFlightsOfStairs(listOfStaircases[i], listOfStaircases[j])
                     distance1 = ConnectingLine1.length
                     distance2 = ConnectingLine2.length
                     if(max(distance1, distance2) < min(listOfStaircases[i].runningLength, listOfStaircases[j].runningLength)):
                         print(distance1, distance2, listOfStaircases[i].runningLength, listOfStaircases[j].runningLength)
                         print("Connection Found!")
 
+                        midLand = [ConnectingLine1, ConnectingLine2, boundingLine1, boundingLine2]
+                        midLand = searchLinesInSearchSpace(midLand, RemainingSearchSpace)
+                        #Do something with the midlanding
+                else:
+
+                    #distanceBetweenFlights1 = min(boundingBox1[1].distance(boundingBox2[0]), boundingBox1[0].distance(boundingBox2[1]))
+                    #distanceBetweenFlights2 = min(boundingBox1[2].distance(boundingBox2[3]), boundingBox1[3].distance(boundingBox2[2]))
+                    leftDistance = boundingBox1[0].distance(Segment(boundingBox2[1], boundingBox2[3]).projection(boundingBox1[0]))
+                    rightDistance = boundingBox1[1].distance(Segment(boundingBox2[0], boundingBox2[2]).projection(boundingBox1[1]))
+                    upperDifference = boundingBox1[0].distance(Segment(boundingBox1[0], boundingBox1[2]).projection(boundingBox2[0]))
+                    lowerDifference = boundingBox1[3].distance(Segment(boundingBox1[0], boundingBox1[2]).projection(boundingBox2[3]))
+                    if(abs(listOfStaircases[i].runningLength - listOfStaircases[j].runningLenth) < maxStairSize and min(leftDistance, rightDistance)<maxStairSize and min(upperDifference, lowerDifference)<maxStairSize):
+                        #--------------------------------------------------------------
+                        configuration = ""
+                        if(leftDistance < rightDistance):
+                            configuration+="l"
+                        else:
+                            configuration+="r"
+                        if(upperDifference<lowerDifference):
+                            configuration+="u"
+                        else:
+                            configuration+="d"
+                        #--------------------------------------------------------------
+                        if(configuration=="lu"):
+                            boundingLine1 = Segment(boundingBox2[0], boundingBox1[1])
+                            connectingLine1, connectingLine2, boundingLine2 = closeMidlanding(boundingLine1, boundingBox1[3], midLandingWidth)
+                        elif(configuration=="ld"):
+                            boundingLine1 = Segment(boundingBox2[2], boundingBox1[3])
+                            connectingLine1, connectingLine2, boundingLine2 = closeMidlanding(boundingLine1, boundingBox1[0], midLandingWidth)
+                        elif(configuration=="ru"):
+                            boundingLine1 = Segment(boundingBox1[0], boundingBox2[1])
+                            connectingLine1, connectingLine2, boundingLine2 = closeMidlanding(boundingLine1, boundingBox1[3], midLandingWidth)
+                        else:
+                            boundingLine1 = Segment(boundingBox1[2], boundingBox2[3])
+                            connectingLine1, connectingLine2, boundingLine2 = closeMidlanding(boundingLine1, boundingBox1[0], midLandingWidth)
+                        midLand = [ConnectingLine1, ConnectingLine2, boundingLine1, boundingLine2]
+                        midLand = searchLinesInSearchSpace(midLand, RemainingSearchSpace)
+                    else:
+                        distanceBetweenCentroids1 = listOfStaircases[i].lineCentroids[0].distance(listOfStaircases[j].lineCentroids[1])
+                        distanceBetweenCentroids2 = listOfStaircases[i].lineCentroids[1].distance(listOfStaircases[j].lineCentroids[0])
+                        if(distanceBetweenCentroids1 > distanceBetweenCentroids2):
+                            seg1 = Segment(boundingBox1[2], boundingBox1[3])
+                            seg2 = Segment(boundingBox2[0], boundingBox2[1])
+
+                        else:
+                            seg1 = Segment(boundingBox1[0], boundingBox1[1])
+                            seg2 = Segment(boundingBox2[2], boundingBox2[3])
+
+                        if(seg1.a.distance(seg2.b) < seg1.b.distance(seg2.a)):
+                            connectingLine1 = Segment(seg1.b, seg2.projection(seg1.b))
+                            connectingLine2 = Segment(seg2.a, seg1.projection(seg2.a))
+                                
+                        else:
+                            connectingLine1 = Segment(seg2.b, seg1.projection(seg2.b))
+                            connectingLine2 = Segment(seg1.a, seg2.projection(seg1.a))
+                        boundingLine1 = Segment(connectingLine1.a, connectingLine2.a)
+                        boundingLine2 = Segment(connectingLine1.b, connectingLine2.b)
+                        if(max(connectingLine1.length, boundingLine1.length) < maxMidLandingLength):
+                            midLand = [connectingLine1, connectingLine2, boundingLine1, boundingLine2]
+                            midLand = searchLinesInSearchSpace(midLand, RemainingSearchSpace)
+
+            elif(areLineSetsPerpendicular(listOfStaircases[i].avgAngleOfRotation, listOfStaircases[j].avgAngleOfRotation)):
+                commonCorner = []
+                for i in range(len(boundingBox1)):
+                    for j in range(len(boundingBox2)):
+                        if(boundingBox1[i].distance(boundingBox2[j]) < maxStairSize):
+                            commonCorner = [i, j]
+                            break
+                if(len(commonCorner)!=0):
+                    linesOfCommonCorner = []
+                    if i==0 or i==1:
+                        linesOfCommonCorner.append(Segment(boundingBox1[0]), Segment(boundingBox1[1]))
+                    else:
+                        linesOfCommonCorner.append(Segment(boundingBox1[2]), Segment(boundingBox1[3]))
+                    if j==0 or j==1:
+                        linesOfCommonCorner.append(Segment(boundingBox2[0]), Segment(boundingBox2[1]))
+                    else:
+                        linesOfCommonCorner.append(Segment(boundingBox2[2]), Segment(boundingBox2[3]))
+
+                    cornerPoint = linesOfCommonCorner[0].extendedintersection(linesOfCommonCorner[1])
+                    linesOfCommonCorner[0] = ExtendLineSegment(linesOfCommonCorner[0], cornerPoint)
+                    linesOfCommonCorner[1] = ExtendLineSegment(linesOfCommonCorner[1], cornerPoint)
+
+                    if(max([line.length for line in linesOfCommonCorner]) < maxMidLandingLength):
+                        refLine = linesOfCommonCorner[0]
+                        l1, l2, l3 = closeMidlanding(refLine, boundingBox1[(i+2)%4], linesOfCommonCorner[1].length)
+                        midLand = [refLine, l1, l2, l3]
+                        midLand = searchLinesInSearchSpace(midLand, RemainingSearchSpace)
+
+            else:
+                commonCorner = []
+                for i in range(len(boundingBox1)):
+                    for j in range(len(boundingBox2)):
+                        if(boundingBox1[i].distance(boundingBox2[j]) < maxStairSize):
+                            commonCorner = [i, j]
+                            break
+                if(len(commonCorner)!=0):
+                    linesOfCommonCorner = []
+                    if i==0 or i==1:
+                        linesOfCommonCorner.append(Segment(boundingBox1[0]), Segment(boundingBox1[1]))
+                    else:
+                        linesOfCommonCorner.append(Segment(boundingBox1[2]), Segment(boundingBox1[3]))
+                    if j==0 or j==1:
+                        linesOfCommonCorner.append(Segment(boundingBox2[0]), Segment(boundingBox2[1]))
+                    else:
+                        linesOfCommonCorner.append(Segment(boundingBox2[2]), Segment(boundingBox2[3]))
+
+                    connectingLinePair1 = [Segment(linesOfCommonCorner[0].a, linesOfCommonCorner[1].a), Segment(linesOfCommonCorner[0].b, linesOfCommonCorner[1].b)]
+                    connectingLinePair2 = [Segment(linesOfCommonCorner[0].a, linesOfCommonCorner[1].b), Segment(linesOfCommonCorner[0].b, linesOfCommonCorner[1].a)]
+
+                    if(max([line.length for line in connectingLinePair1]) < max([line.length for line in connectingLinePair2])):
+                        connectingLinePair = connectingLinePair1
+                    else:
+                        connectingLinePair = connectingLinePair2
+
+                    if(max([line.length for line in connectingLinePair]) < maxMidLandingLength):
+                        midLand = connectingLinePair + linesOfCommonCorner
+                        midLand = searchLinesInSearchSpace(midLand, RemainingSearchSpace)
                     
-                    
+            if(len(midLand)!=0):
+                pairs.append(set([midLanding(midLand), listOfStaircases[i], listOfStaircases[j]]))
+
+    print(pairs)
+
+    listToPrint = []
+    for i in pairs[0]:
+        listToPrint+=i.listOfLines
+    MakeShapeFile(listToPrint, "Staircase.shp")
+    if(len(pairs) > 1):
+        i = 0
+        j = 1
+        while(i<len(pairs)-1):
+            j = i+1
+            while(j<len(pairs)):
+                if(len(pairs[i] &  pairs[j]) != 0):
+                    temp = pairs.pop(j)
+                    pairs[i] = pairs[i].union(pairs[j])
+                else:
+                    j+=1
+            i+=1
+
+    print(pairs)
+
+            
